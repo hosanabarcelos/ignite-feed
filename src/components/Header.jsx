23b58d59af85cfd01940220e8e3ee65f29598902
Hosana <hosanabarceloscandido@gmail.com>
2023-02-19 15:13:26 -0300

    feat: cria componente header

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import styles from './Header.module.css';
+import logo from '../assets/images/logo.svg';
+
+export function Header() {
+    return (
+        <header className={styles.header}>
+            <img 
+                src={logo} 
+                alt="Logo Ignite"
+            />
+        </header>
+    );
+}
