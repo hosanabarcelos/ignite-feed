e83ecb61969d2277f6c907e46f8343656543559f
Hosana <hosanabarceloscandido@gmail.com>
2023-02-19 16:19:36 -0300

    feat: consome retorno de posts dinâmicos

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,39 +1,52 @@
+import { format, formatDistanceToNow } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Comment } from './Comment';
 import { Avatar } from './Avatar';
 
 import styles from './Post.module.css';
 
-export function Post() {
+export function Post({author, publishedAt, content}) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR,
+    });
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true, 
+    });
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
                     <Avatar 
-                        src="https://github.com/hosanabarcelos.png"
+                        src={author.avatarUrl}
                     />
 
                     <div className={styles.authorInfo}>
-                        <strong>Hosana Barcelos</strong>
-                        <span>Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
                 <time 
-                    title="19 de Fevereiro às 13:30h" 
-                    dateTime="2023-02-19"
+                    title={publishedDateFormatted} 
+                    dateTime={publishedAt.toISOString()}
                 >
-                    Publicado há 2h
+                    {publishedDateRelativeToNow}
                 </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-
-                <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
-
-                <p>👉 jane.design/doctorcare</p>
-
-                <p>#novoprojeto #nlw #rocketseat </p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p>{line.content}</p>
+                    } 
+                    else if (line.type === 'link') {
+                        return <p><a href="#">{line.content}</a></p>  
+                    } 
+                })}
             </div>
 
             <form className={styles.commentForm}>
